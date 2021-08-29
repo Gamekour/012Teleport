720ccd65aba3fdafe074cc9ef0542ec8dff9f9f2
Gamekour <49810363+Gamekour@users.noreply.github.com>
2021-08-29 10:39:42 -0400

    Add files via upload

--- TPKeycard.cs
+++ TPKeycard.cs
@@ -6,6 +6,7 @@ using Exiled.CustomItems.API.Features;
 using Exiled.CustomItems.API.Spawn;
 using Exiled.API.Features.Items;
 using Exiled.Events.EventArgs;
+using Exiled.API.Enums;
 using UnityEngine;
 using MEC;
 
@@ -13,28 +14,15 @@ namespace _012Teleport
 {
     public class TPKeycard : CustomWeapon
     {
-        List<String> KeywordsToFilter = new List<string>();
         public override void Init()
         {
             Exiled.Events.Handlers.Player.PickingUpItem += OnPickingUpItem;
-            Exiled.Events.Handlers.Player.Shooting += OnShooting;
-
-            KeywordsToFilter.Add("Tunnel");
-            KeywordsToFilter.Add("Pocket");
-            KeywordsToFilter.Add("Hid");
-            KeywordsToFilter.Add("106");
-            KeywordsToFilter.Add("LCZ_Curve");
-            KeywordsToFilter.Add("939");
-            KeywordsToFilter.Add("Outside");
-            KeywordsToFilter.Add("HCZ_Curve");
-            KeywordsToFilter.Add("Shelter");
 
             base.Init();
         }
         public override void Destroy()
         {
             Exiled.Events.Handlers.Player.PickingUpItem -= OnPickingUpItem;
-            Exiled.Events.Handlers.Player.Shooting -= OnShooting;
 
             base.Destroy();
         }
@@ -58,33 +46,43 @@ namespace _012Teleport
                 Spawn(ev.Pickup.Position);
                 Room[] roomObjects = (Room[])UnityEngine.Object.FindObjectsOfType(typeof(Room));
                 List<Room> roomsFiltered = new List<Room>();
+                Log.Debug(roomObjects.Length);
 
                 foreach (Room room in roomObjects)
                 {
-                    if (room.Zone == Exiled.API.Enums.ZoneType.Surface || room.Zone == Exiled.API.Enums.ZoneType.Unspecified)
-                        return;
-                    foreach(string keyword in KeywordsToFilter)
+                    if (room.Zone != ZoneType.Surface && room.Zone != ZoneType.Unspecified)
                     {
-                        if(!room.name.Contains(keyword))
+                        if (
+                            room.Type != RoomType.Pocket &&
+                            room.Type != RoomType.HczHid &&
+                            room.Type != RoomType.Hcz939 &&
+                            room.Type != RoomType.Hcz106 &&
+                            room.Type != RoomType.LczCurve &&
+                            room.Type != RoomType.HczCurve &&
+                            room.Type != RoomType.EzCollapsedTunnel &&
+                            room.Type != RoomType.EzVent
+                            )
                         {
                             roomsFiltered.Add(room);
-                            Log.Debug(room.Name);
+                            //Log.Debug(room.Name);
                         }
                     }
-                        
                 }
                 Room randomRoom = roomsFiltered[UnityEngine.Random.Range(0, roomsFiltered.Count - 1)];
-                ev.Player.Position = randomRoom.Position + Vector3.up * 1.5f;
-                ev.Player.Hurt(ev.Player.Health * 0.25f);
-                ev.Player.Broadcast(10, "<color=#9e0000><size=30><b>YOU HAVE CONSUMED SCP-012!\nYOU HAVE BEEN TELEPORTED TO A RANDOM LOCATION BY SCP-012!</b></size></color>");
                 Timing.CallDelayed(0.5f, () =>
                 {
+                    ev.Player.Position += Vector3.up * -10f;
                     foreach (Item item in ev.Player.Items)
                     {
                         if (Check(item))
-                            ev.Player.RemoveItem(item);
+                        {
+                            ev.Player.DropItem(item);
+                        }
                     }
                 });
+                Timing.CallDelayed(0.6f, () => ev.Player.Position = randomRoom.Position + Vector3.up * 1.5f);
+                ev.Player.Hurt(ev.Player.Health * 0.25f);
+                ev.Player.Broadcast(10, "<color=#9e0000><size=30><b>YOU HAVE CONSUMED SCP-012!\nYOU HAVE BEEN TELEPORTED TO A RANDOM LOCATION BY SCP-012!</b></size></color>");
             }
         }
     }
